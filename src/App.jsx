d15155409467ec7ffaf595b9f10cfa0fbb261090
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-11-08 21:12:07 +0200

    alomost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-
 import { interval } from 'rxjs';
-import React, { useEffect, useState, useCallback } from 'react'
-import {  Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import { Button, ButtonGroup, Card, CardActions, CardContent, Container, Grid, Typography } from '@material-ui/core';
-import useStyles from './style.js'
+import React, { useEffect, useState, useCallback } from 'react';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import useStyles from './style.js';
 
 export default function App() {
   const [sec, setSec] = useState(0);
-  const [status, setStatus] = useState("stop");
-  const [isOn, setIsOn] = useState(false)
-  const classes = useStyles()
- 
+  const [status, setStatus] = useState('stop');
+  const [isOn, setIsOn] = useState(false);
+  const classes = useStyles();
+
   useEffect(() => {
     const unsubscribe$ = new Subject();
     interval(1000)
       .pipe(takeUntil(unsubscribe$))
       .subscribe(() => {
-        if (status === "run") {
+        if (status === 'run') {
           setSec(val => val + 1000);
         }
       });
@@ -26,41 +34,45 @@ export default function App() {
       unsubscribe$.complete();
     };
   }, [status]);
- 
+
   const start = useCallback(() => {
-    setStatus("run");
-    setIsOn(true)
+    setStatus('run');
+    setIsOn(true);
   }, []);
- 
+
   const stop = useCallback(() => {
-    setStatus("stop");
+    setStatus('stop');
     setSec(0);
-    setIsOn(false)
+    setIsOn(false);
   }, []);
- 
+
   const reset = useCallback(() => {
     setSec(0);
   }, []);
- 
+
   const wait = useCallback(() => {
-    setStatus("wait");
-    setIsOn(false)
+    setStatus('wait');
+    setIsOn(false);
   }, []);
- 
+
   console.log(new Date(sec));
   return (
-    <Container className={classes.cardGrid} maxWidth='lg'>
-      <Grid container justify='center'>
+    <Container className={classes.cardGrid} maxWidth="lg">
+      <Grid container justify="center">
         <Grid item xs={12} sm={12} md={8} lg={6}>
           <Card className={classes.card}>
             <CardContent className={classes.cardContent}>
-              <Typography variant='h1'>{new Date(sec).toISOString().slice(11, 19)}</Typography>
+              <Typography variant="h1">
+                {new Date(sec).toISOString().slice(11, 19)}
+              </Typography>
             </CardContent>
             <CardActions>
-              <ButtonGroup align='center' variant='contained' color='primary'>
-                {!isOn
-                  ?<Button onClick={start}>Start</Button>
-                  :<Button onClick={stop}>Stop</Button>}
+              <ButtonGroup align="center" variant="contained" color="primary">
+                {!isOn ? (
+                  <Button onClick={start}>Start</Button>
+                ) : (
+                  <Button onClick={stop}>Stop</Button>
+                )}
                 <Button onClick={reset}>Reset</Button>
                 <Button onClick={wait}>Wait</Button>
               </ButtonGroup>
